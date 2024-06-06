5a863740723ebe13e9669c027fa7a7c98af53964
Malith Kuruppu <info.mkappworks@gmail.com>
2024-06-06 21:01:20 +0200

    tech: Update database connection string in drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from "drizzle-kit";
 
+require("dotenv").config({ path: ".env.local" });
+
 export default defineConfig({
   schema: "./src/db/schemas/*.ts",
   out: "./src/db/migrations/drizzle",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DATABASE_CONNECTION_STRING!,
+    url: process.env.DB_CONNECTION_STRING!,
   },
   verbose: true,
   strict: true,
