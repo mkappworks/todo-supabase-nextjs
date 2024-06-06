2d81ae276a7be954bc530a7a5c29fedd90b34ef2
Malith Kuruppu <info.mkappworks@gmail.com>
2024-06-06 20:27:48 +0200

    tech: Add providers and theme provider components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,33 +1,40 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter as FontSans } from "next/font/google";
 
 import { Toaster } from "react-hot-toast";
 
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+import { Providers } from "@/providers";
+
+import { cn } from "@/lib/utils";
 
 export const metadata: Metadata = {
   title: "TodoNow",
   description: "Todo app",
 };
 
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        {children}
-        <Toaster
-          toastOptions={{
-            style: {
-              textAlign: "center",
-            },
-          }}
-        />
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={cn(
+          "bg-background min-h-screen font-sans antialiased",
+          fontSans.variable,
+        )}
+      >
+        <Providers>
+          {children}
+          <Toaster />
+        </Providers>
       </body>
     </html>
   );
