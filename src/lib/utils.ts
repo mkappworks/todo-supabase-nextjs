38826e1e0b79207be18a3bc14bbe6e12db1df1c4
Malith Kuruppu <info.mkappworks@gmail.com>
2024-06-06 21:22:51 +0200

    feat: Add user authentication actions

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,14 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function getErrorMessage(
+  error: unknown,
+  defaultMessage: string = "Something went wrong",
+) {
+  let errorMessage = defaultMessage;
+  if (error instanceof Error && error.message.length > 100)
+    errorMessage = error.message;
+
+  return errorMessage;
+}
