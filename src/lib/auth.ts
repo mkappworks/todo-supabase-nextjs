7ba5feac3370925a89b46e98623c884ed1e480f0
Malith Kuruppu <info.mkappworks@gmail.com>
2024-06-06 21:13:14 +0200

    feat: Add supabase server auth

---
+++ src/lib/auth.ts
@@ -0,0 +1,40 @@
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+import { createServerClient, type CookieOptions } from "@supabase/ssr";
+
+export const getUser = async () => {
+  const auth = getSupabaseAuth();
+  const user = (await auth.getUser()).data.user;
+  if (!user) redirect("/login");
+
+  return user;
+};
+
+export function getSupabaseAuth() {
+  const cookieStore = cookies();
+
+  const superbaseClient = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name: string) {
+          return cookieStore.get(name)?.value;
+        },
+        set(name: string, value: string, options: CookieOptions) {
+          try {
+            cookieStore.set({ name, value, ...options });
+          } catch (error) {}
+        },
+        remove(name: string, options: CookieOptions) {
+          try {
+            cookieStore.set({ name, value: "", ...options });
+          } catch (error) {}
+        },
+      },
+    },
+  );
+
+  return superbaseClient.auth;
+}
