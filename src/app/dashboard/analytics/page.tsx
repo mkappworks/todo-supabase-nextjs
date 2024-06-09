24cf5dcdf971f4789ad9471fa3f07410e6880b62
Malith Kuruppu <info.mkappworks@gmail.com>
2024-06-09 21:33:14 +0200

    feat: Add analytics, settings, and todos pages

---
+++ src/app/dashboard/analytics/page.tsx
@@ -0,0 +1,7 @@
+export default function AnalyticsPage() {
+  return (
+    <div className="flex min-h-screen flex-col items-center px-4 pb-24">
+      Analytics Page
+    </div>
+  );
+}
---
+++ src/app/dashboard/todos/page.tsx
@@ -0,0 +1,7 @@
+export default function TodosPage() {
+  return (
+    <div className="flex min-h-screen flex-col items-center px-4 pb-24">
+      Todos List
+    </div>
+  );
+}
