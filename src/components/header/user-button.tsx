8c6a971c224c393c80fc8bf5ac1557a9f41c661e
Malith Kuruppu <info.mkappworks@gmail.com>
2024-06-09 21:26:11 +0200

    feat: Add dashboard header, sidenav anduser profile dropdown menu

---
+++ src/components/header/user-button.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+import { signOutAction } from "@/actions/user";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { User } from "@supabase/supabase-js";
+import toast from "react-hot-toast";
+
+import { Button } from "../ui/button";
+
+type Props = {
+  user: User;
+  className?: string;
+};
+
+export function UserButton({ user }: Props) {
+  const router = useRouter();
+
+  const handleSignOut = async () => {
+    const toastId = toast.loading("Singing out...");
+    await signOutAction();
+    router.replace("/sign-in");
+    toast.dismiss(toastId);
+  };
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="outline"
+          size="icon"
+          className="overflow-hidden rounded-full"
+        >
+          <Image
+            src="/placeholder-user.jpg"
+            width={36}
+            height={36}
+            alt="Avatar"
+            className="overflow-hidden rounded-full"
+          />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuLabel>{user.email}</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>Settings</DropdownMenuItem>
+        <DropdownMenuItem>Support</DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem onClick={handleSignOut}>SignOut</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
+
+export default UserButton;
