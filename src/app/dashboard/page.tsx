546decfd025fd0daf1be4036b899f900d351f429
Malith Kuruppu <info.mkappworks@gmail.com>
2024-06-19 16:47:00 +0200

    refactor: Update DashboardPage to redirect to "/dashboard/todos"

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,5 @@
+import { redirect } from "next/navigation";
+
 export default function DashboardPage() {
-  return (
-    <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
-      Main Content
-    </main>
-  );
+  redirect("/dashboard/todos");
 }
